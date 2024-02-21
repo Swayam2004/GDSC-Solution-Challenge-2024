150a49758cccf1e4a1a3bce1a8d65fc4d62c8989
Swayam2004 <swayamsudhabbsr3@gmail.com>
2024-02-22 00:38:19 +0530

    fix the annoying error with the SignupScreen() function

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,115 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:gdsc_solution_challenge_2024/screens/sign_in.dart';
 import 'package:gdsc_solution_challenge_2024/widgets/round_button.dart';
 
-class SignupScreen extends StatefulWidget{
-  const SignupScreen({Key?key}):super(key:key);
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
   State<SignupScreen> createState() => _SignupScreenState();
 }
-class SigninScreen extends StatefulWidget{
-  const SigninScreen({Key?key}):super(key:key);
+// class SigninScreen extends StatefulWidget{
+//   const SigninScreen({Key?key}):super(key:key);
 
-  @override
-  State<SigninScreen> createState() => _SigninScreenState();
-}
+//   @override
+//   State<SigninScreen> createState() => _SigninScreenState();
+// }
 
-class _SignupScreenState extends State<SignupScreen>{
+class _SignupScreenState extends State<SignupScreen> {
   final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   @override
-  void dispose(){
+  void dispose() {
     super.dispose();
     emailController.dispose();
     passwordController.dispose();
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: Text('Sign Up'),
+        title: const Text('Sign Up'),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20  ),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-              controller: emailController,
-              decoration:const InputDecoration(
-                hintText: 'Email',
-                helperText: 'enter your email',
-                prefixIcon: Icon(Icons.alternate_email)
-              ),
-              validator: (value){
-                if(value!.isEmpty){
-                  return 'Enter email';
-                }
-                return null;
-
-              },
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                          hintText: 'Email',
+                          helperText: 'enter your email',
+                          prefixIcon: Icon(Icons.alternate_email)),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: 'Password',
+                          helperText: 'enter your password',
+                          prefixIcon: Icon(Icons.lock_open)),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                )),
+            const SizedBox(
+              height: 50,
             ),
-
-            
-            SizedBox(height:20 ,),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              controller: passwordController,
-              obscureText: true,
-              decoration:const InputDecoration(
-                hintText: 'Password',
-                helperText: 'enter your password',
-                prefixIcon: Icon(Icons.lock_open)
-              ),
-              validator: (value){
-                if(value!.isEmpty){
-                  return 'Enter password';
-
-                }
-                return null;
-
-              },
-            ),
-                ],
-                   
-
-
-                
-                )
-              ),
-             const SizedBox(height:50 ,),
-           
             RoundButton(
-              title : 'Sign up',
-              
-              onTap:(){
-                if(_formKey.currentState!.validate()){
-
-                }
-
-              } ,
+              title: 'Sign up',
+              onTap: () {
+                if (_formKey.currentState!.validate()) {}
+              },
             ),
-            const SizedBox(height:30 ,),
+            const SizedBox(
+              height: 30,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Already have an account?'),
-                TextButton(onPressed: (){
-                  Navigator.push(context,
-                   MaterialPageRoute(
-                    builder: (context) => SigninScreen())
-                    ); 
-                },
-                child: Text('Sign In'))
+                const Text('Already have an account?'),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return const SigninScreen();
+                      }));
+                    },
+                    child: const Text('Sign In'))
               ],
             )
           ],
